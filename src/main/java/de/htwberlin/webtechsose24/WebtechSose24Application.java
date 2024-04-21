4999b7ef5faeb3851c46d3d0aeeaa1c0ebcc64d9
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-04-21 20:19:34 +0200

    initial

---
+++ src/main/java/de/htwberlin/webtechsose24/WebtechSose24Application.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechsose24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechSose24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechSose24Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtechsose24/WebtechSose24ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechsose24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechSose24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

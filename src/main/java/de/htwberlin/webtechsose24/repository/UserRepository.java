03b0110ab7a695bc4538235eb891442de54806c0
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-06-11 17:26:40 +0200

    create user repository

---
+++ src/main/java/de/htwberlin/webtechsose24/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package de.htwberlin.webtechsose24.repository;
+
+import de.htwberlin.webtechsose24.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}

0485a22158cf250cf6aa248acb265506374191b7
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-06-27 17:19:17 +0200

    add methods to find date by filter

--- src/main/java/de/htwberlin/webtechsose24/controller/ScoreController.java
+++ src/main/java/de/htwberlin/webtechsose24/controller/ScoreController.java
@@ -5,6 +5,7 @@ import de.htwberlin.webtechsose24.repository.ScoreRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -21,6 +22,24 @@ public class ScoreController {
     @GetMapping
     public List<Score> getAllScores() {
         return scoreRepository.findAll(Sort.by(Sort.Direction.ASC, "timeInSeconds"));
+    }
+
+    @GetMapping("/day/{difficulty}")
+    public List<Score> getScoresLastDay(@PathVariable String difficulty) {
+        LocalDateTime oneDayAgo = LocalDateTime.now().minusDays(1);
+        return scoreRepository.findByCreatedAtAfterAndDifficulty(oneDayAgo, difficulty);
+    }
+
+    @GetMapping("/week/{difficulty}")
+    public List<Score> getScoresLastWeek(@PathVariable String difficulty) {
+        LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(1);
+        return scoreRepository.findByCreatedAtAfterAndDifficulty(oneWeekAgo, difficulty);
+    }
+
+    @GetMapping("/month/{difficulty}")
+    public List<Score> getScoresLastMonth(@PathVariable String difficulty) {
+        LocalDateTime oneMonthAgo = LocalDateTime.now().minusMonths(1);
+        return scoreRepository.findByCreatedAtAfterAndDifficulty(oneMonthAgo, difficulty);
     }
 
     @PostMapping
--- src/main/java/de/htwberlin/webtechsose24/repository/ScoreRepository.java
+++ src/main/java/de/htwberlin/webtechsose24/repository/ScoreRepository.java
@@ -4,6 +4,10 @@ import de.htwberlin.webtechsose24.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Long> {
+    List<Score> findByCreatedAtAfterAndDifficulty(LocalDateTime date, String difficulty);
 }

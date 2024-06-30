8d8b9b0e725ca27c6ed03888c3167aded00a1f81
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-06-30 23:46:47 +0200

    webconfig

---
+++ src/main/java/WebConfig.java
@@ -0,0 +1,20 @@
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "https://minesweeper-sidoma.onrender.com");
+
+
+    }
+}
